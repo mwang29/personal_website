1151ef79df820bfcaa27b4a032ecdb24a8543c3a
Michael Wang <michael20995@gmail.com>
2020-03-13 21:22:17 -0400

    made boa field nonoptional

--- forms.py
+++ forms.py
@@ -48,8 +48,7 @@ class CreditCardForm(FlaskForm):
     amazon_member = BooleanField('Amazon Member:')
     costco_member = BooleanField('Costco Member:')
     sams_member = BooleanField("Sam's Club Member:")
-    boa_amt = DecimalField('Capital in existing Bank of America accounts (optional):',
-                         )
+    boa_amt = DecimalField('Capital in existing Bank of America accounts', validators=[InputRequired()])
 
     submit = SubmitField('Calculate!')
 
